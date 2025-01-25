6011447efb48babdbf60929175dd4a9d2ebd551a
Rodrigo Natan <rnattan82@gmail.com>
2025-01-25 11:55:43 -0300

    Criando componente Header

--- peixe-frio/src/components/Header/index.tsx
+++ peixe-frio/src/components/Header/index.tsx
@@ -1,10 +1,13 @@
 import styles from "./Header.module.scss"
+import logo from "../../../public/logo.png"
 
 function Header() {
     return (
-        <div className={styles.container}>
-            <h1>Header</h1>
-        </div>
+        <header className={styles.header}>
+            <div>
+                <img className={styles.logo} src="/logo.png" alt="Logo" />
+            </div>
+        </header>
     )
 }
 
