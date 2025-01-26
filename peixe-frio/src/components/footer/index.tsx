5c590864362b72f185399cc968d79f2d27edafff
Rodrigo Natan <rnattan82@gmail.com>
2025-01-26 15:58:45 -0300

    Criando o component Footer

---
+++ peixe-frio/src/components/footer/index.tsx
@@ -0,0 +1,46 @@
+import styles from "./Footer.module.scss"
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <div className={styles.left_container}>
+                <h2>Departamentos</h2>
+                <span>Início</span>
+                <span>Perfil</span>
+                <span>Dúvidas</span>
+
+            </div>
+
+            <div className={styles.mid_container}>
+                <h2>Entre em Contato</h2>
+
+                <div className="wpp_div">
+                    <img className={styles.wpp_img} src="./wpp-img.png" alt="local-img" />
+                    <span>+55 (75)9 9964-0722</span>
+                </div>
+
+                <div className="email_div">
+                    <img className={styles.email_img} src="./email_img.png" alt="email-img" />
+                    <span>djalmamatogrosso@gmail.com</span>
+                </div>
+
+                <div className="local_div">
+                    <img className={styles.local_img} src="./local_img.png" alt="" />
+                    <span>Maragogipe/BA</span>
+                </div>
+
+            </div>
+            <div className={styles.right_container}>
+                <h2>Permaneça Conectado</h2>
+                <div className={styles.social_midias}>
+                    <img className={styles.insta_img} src="./insta_img.png" alt="instagram-img" />
+                    <img className={styles.face_img} src="./face_img.png" alt="facebook-img" />
+
+                </div>
+            </div>
+
+        </footer>
+    )
+}
+
+export default Footer;
--- peixe-frio/src/pages/index.tsx
+++ peixe-frio/src/pages/index.tsx
@@ -1,11 +1,13 @@
 
 import styles from "@/styles/Home.module.scss";
 import Header from "@/components/Header"
+import Footer from "@/components/footer";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   )
 }
