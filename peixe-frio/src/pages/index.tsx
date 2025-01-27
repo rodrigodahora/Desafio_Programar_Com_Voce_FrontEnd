9ea8232766f02f35429d53068ee9a378a55495ee
Rodrigo Natan <rnattan82@gmail.com>
2025-01-27 18:57:07 -0300

    Criando Modal para identificação do produto, criando estilo de botão

--- peixe-frio/src/pages/index.tsx
+++ peixe-frio/src/pages/index.tsx
@@ -2,34 +2,37 @@ import styles from "@/styles/Home.module.scss";
 import Layout from "@/components/Layout";
 import Product from "@/components/Product";
 import Side from "@/components/Side";
+import Modal from "@/components/Modal"
+import { useState } from "react";
 
 
 
 export default function Home() {
+  const [open, setOpen] = useState(false);
+
+  function hendleSelectProduct() {
+    setOpen(true);
+  }
+
+  function handleClose() {
+    setOpen(false);
+  }
   return (
     <Layout>
-      <div className={styles.container_main}>
+      <>
 
-        <div className={styles.side_container}>
-          <Side />
+        <div className={styles.container_main}>
+
+          <div className={styles.side_container}>
+            <Side />
+          </div>
+          <div className={styles.product_container}>
+            <Product handleSelectProduc={handleSelectProduct} />
+
+          </div>
         </div>
-
-        <div className={styles.product_container}>
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-        </div>
-
-      </div>
+        <Modal open={open} handleClose={handleClose} />
+      </>
     </Layout>
 
   )
