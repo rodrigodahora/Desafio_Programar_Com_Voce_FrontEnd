05517d05d3cb4f592264c5ee61612b8a5e70eb28
Rodrigo Natan <rnattan82@gmail.com>
2025-02-05 22:19:17 -0300

    Configurando versões compativeis do next e react

---
+++ peixe-frio/next.config.mjs
@@ -0,0 +1,5 @@
+export default {
+  images: {
+    domains: ['blogger.googleusercontent.com', 'oreinodopeixe.com.br', 'static.wixstatic.com', 'images.tcdn.com.br', 'orcapescados.com.br', 'img77.uenicdn.com'],
+  },
+};
