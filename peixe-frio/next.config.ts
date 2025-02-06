a255c85fdfc055e5478518a3a3519c91b6190bb2
Rodrigo Natan <rnattan82@gmail.com>
2025-02-05 21:36:14 -0300

    Verificando next.congit.ts

--- peixe-frio/next.config.ts
+++ peixe-frio/next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     domains: ['blogger.googleusercontent.com', 'oreinodopeixe.com.br', 'static.wixstatic.com', 'images.tcdn.com.br', 'orcapescados.com.br', 'img77.uenicdn.com'],
-  }
+  },
+  webpack(config) {
+    // Personalizações de Webpack podem ser feitas aqui se necessário
+    return config;
+  },
 };
 
 export default nextConfig;
